93dafa7ebe331271273cb7aaabc90e26cb4d252e
ElliBelli66 <50132411+ElliBelli66@users.noreply.github.com>
2023-09-30 11:45:35 +0200

    Update sv_main.lua

--- eb_våbenhandler/server/sv_main.lua
+++ eb_våbenhandler/server/sv_main.lua
@@ -10,15 +10,28 @@ end)
 
 RegisterNetEvent('eb:wepdealer_giveweapon', function(label, spawncode, craftprice, craftamount)
     local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.getJob().name == Config.Job then
-        TriggerEvent('esx_addonaccount:getSharedAccount', 'society_' .. Config.Job, function(account)
-            account.removeMoney(craftprice)
-            xPlayer.addInventoryItem(spawncode, craftamount)
-            TriggerClientEvent('eb:wepdealer_craftsuccess', source, label, craftprice, craftamount)
-        end)
-    else
-        print('[ID: ' .. source .. ' ] udførte en jobhandling fra et job som personen ikke har!')
+    if xPlayer.getJob().name ~= Config.Job then
+      return print('[ID: ' .. source .. ' ] udførte en jobhandling fra et job som personen ikke har!')
     end
+
+    local found = false
+    for _, v in pairs(Config.WeaponCraft.Weplist) do
+      if v.label == spawncode 
+      and v.craftprice == craftprice 
+      and v.craftamount == craftamount then
+        found = true
+      end
+    end
+
+    if not found then
+      return print("[ID: " .. source .. " ] Er muligvis en modder som prøvede og spawne items")
+    end
+
+    TriggerEvent('esx_addonaccount:getSharedAccount', 'society_' .. Config.Job, function(account)
+        account.removeMoney(craftprice)
+        xPlayer.addInventoryItem(spawncode, craftamount)
+        TriggerClientEvent('eb:wepdealer_craftsuccess', source, label, craftprice, craftamount)
+    end)
 end)
 
 TriggerEvent('esx_society:registerSociety', Config.Job, Config.Job, 'society_' .. Config.Job, 'society_' .. Config.Job, 'society_' .. Config.Job, {type = 'private'})
