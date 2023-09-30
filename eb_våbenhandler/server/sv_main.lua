ba39242049bb9ca4cec654292363b757ef3e3ad4
ElliBelli66 <50132411+ElliBelli66@users.noreply.github.com>
2023-09-30 12:10:00 +0200

    Update sv_main.lua

--- eb_våbenhandler/server/sv_main.lua
+++ eb_våbenhandler/server/sv_main.lua
@@ -10,24 +10,15 @@ end)
 
 RegisterNetEvent('eb:wepdealer_giveweapon', function(label, spawncode, craftprice, craftamount)
     local xPlayer = ESX.GetPlayerFromId(source)
+        
     if xPlayer.getJob().name ~= Config.Job then
-      return print('[ID: ' .. source .. ' ] udførte en jobhandling fra et job som personen ikke har!')
+        return print('[ID: ' .. source .. ' ] udførte en jobhandling fra et job som personen ikke har!')
     end
-
-    local found = false
-    for _, v in pairs(Config.WeaponCraft.Weplist) do
-      if v.label == spawncode 
-      and v.craftprice == craftprice 
-      and v.craftamount == craftamount then
-        found = true
-      end
-    end
-
-    if not found then
-      return print("[ID: " .. source .. " ] Er muligvis en modder som prøvede og spawne items")
-    end
-
+        
     TriggerEvent('esx_addonaccount:getSharedAccount', 'society_' .. Config.Job, function(account)
+        if not account then
+            return print('[ID: ' .. source .. ' ] Ugyldig konto.')
+        end
         account.removeMoney(craftprice)
         xPlayer.addInventoryItem(spawncode, craftamount)
         TriggerClientEvent('eb:wepdealer_craftsuccess', source, label, craftprice, craftamount)
